5338b97e0710ee244fe72fa600a7d51d047653ac
Mohammad Akram <khanakram32392@gmail.com>
2025-04-29 23:47:00 +0530

    remove proxy from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy: {
-      '/api':  {
-        target: "https://social-media-backend-1-vek9.onrender.com", // Backend URL
-        changeOrigin: true,
-        secure: false,
-      },
-    }
-  },
   plugins: [
     react(), 
     tailwindcss()
