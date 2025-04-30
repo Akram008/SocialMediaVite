2fac4935a35e8610f273341891826527d819dafb
Mohammad Akram <khanakram32392@gmail.com>
2025-04-30 18:39:27 +0530

    add in protected Route

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+const API_BASE = import.meta.env.VITE_API_BASE_URL
 import Cookies from 'js-cookie'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const ProtectedRoute = () => {
-  const [accessGiven, setAccessGiven] = useState(true)
-  const token = Cookies.get('accessToken')
+  const [accessGiven, setAccessGiven] = useState(false)
+  useEffect(()=>{
+    const fetchAuthentication = async() => {
+      const isAuthenticateRes = await axios.get(`${API_BASE}/api/v1/users/me`, {withCredentials: true})
+      isAuthenticateRes.data.success ? setAccessGiven(true) : setAccessGiven(false)
+    }
+  
+    console.log(isAuthenticateRes)
+    fetchAuthentication()
+  },[])
 
-  console.log(token)
 
   return accessGiven ? <Outlet/> : <Navigate to='/login' replace/> 
 }
