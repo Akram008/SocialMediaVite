c8ee66cd6ed09e772ed94123bbaf1abf6a5655ad
Mohammad Akram <khanakram32392@gmail.com>
2025-04-30 15:53:36 +0530

    add API BASE to like

--- src/components/FeedContainer/FeedContainer.jsx
+++ src/components/FeedContainer/FeedContainer.jsx
@@ -36,7 +36,7 @@ const FeedContainer = (props) => {
   const handleLike = async () =>{
     const likeResponse = await axios.post(`${API_BASE}/api/v1/likes/toggleLike/${feed._id}`, {withCredentials: true}) 
 
-    const isLikedResponse = await axios.get(`/api/v1/likes/isLiked/${feed._id}`, {withCredentials: true})
+    const isLikedResponse = await axios.get(`${API_BASE}/api/v1/likes/isLiked/${feed._id}`, {withCredentials: true})
     setIsLiked(isLikedResponse.data.data)
   }
 
