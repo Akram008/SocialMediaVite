eff4302c15c73a9620f9cddea6ca1ed5900dcdfd
Mohammad Akram <khanakram32392@gmail.com>
2025-05-11 12:16:15 +0530

    add post delete functions

--- src/components/FeedContainer/ProfilePostContainer.jsx
+++ src/components/FeedContainer/ProfilePostContainer.jsx
@@ -18,6 +18,8 @@ const ProfilePostContainer = (props) => {
   const [totalComments, setTotalcomments] = useState(0)
   const [showComments, setShowComments] = useState(false)
   const [editBtn, setEditBtn] = useState(false)
+  const [showDeletePopup, setShowDeletePopup] = useState(false)
+  const [isDeleted, setIsDeleted] = useState(false)
 
   useEffect(()=>{
     const fetchLike = async ()=>{
@@ -41,6 +43,8 @@ const ProfilePostContainer = (props) => {
     try {
       const res = await axios.delete(`${API_BASE}/api/v1/posts/deletePost/${post._id}`, {withCredentials: true})
       console.log(res.data)
+      setIsDeleted(true)
+      setShowDeletePopup(false)
     } catch (error) {
       console.log(`post deletion error: ${error}`)
     } 
@@ -75,10 +79,40 @@ const ProfilePostContainer = (props) => {
     return new Date(timestamp);
   }
 
+  const isDeletePopup = () => (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/75 z-50 p-5">
+          <div className="bg-[#121212] p-6 rounded-lg shadow-lg max-w-sm w-full text-center">
+            <h2 className="text-xl font-semibold mb-4 text-white">Confirmation to delete this post!</h2>
+            <button onClick={handleDelelte}
+              className="px-4 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-700"
+            >
+              Yes
+            </button>
+            <button onClick={()=> setShowDeletePopup(false)}
+              className="px-4 py-2 bg-transparent text-white border-none rounded"
+            >
+              Cancel
+            </button>
+          </div>
+      </div>
+  )
+
+  const deletedPostContainer = () => (
+    <div className='w-full bg-stone-400 p-3'>
+      <p className='text-gray-900'>This post is successfully deleted!</p>
+    </div>
+  )
+
   return (
     <div className='w-full flex flex-col mb-8'>
         <p className='text-lg text-[#686767] font-light italic self-start'>{postDate}</p>
-        <div className='w-full bg-[#333333] rounded-lg flex flex-col p-5 gap-5'>
+        {showDeletePopup && isDeletePopup()}
+        {isDeleted 
+          ? 
+            deletedPostContainer()
+          : 
+          (
+          <div className='w-full bg-[#333333] rounded-lg flex flex-col p-5 gap-5'>
                 <div className='w-full flex flex-row justify-between items-center '>
                     <h3 className='text-[#faf9f6] w-30 font-bold'>{post.title}</h3>
                     <p className='text-[#faf9f6] opacity-50'>Type: {post.postType}</p>
@@ -98,7 +132,7 @@ const ProfilePostContainer = (props) => {
                     </div>
 
                     <div className='flex items-center gap-2'>
-                      {editBtn && <button className='text-md border-1 border-red-600 rounded-lg p-2 text-red-600 bg-white' onClick={handleDelelte}>
+                      {editBtn && <button className='text-md border-1 border-red-600 rounded-lg p-2 text-red-600 bg-white' onClick={() => setShowDeletePopup(true)}>
                         <FaRegTrashAlt/>
                       </button>}
 
@@ -111,7 +145,9 @@ const ProfilePostContainer = (props) => {
                       </button>
                     </div>
                 </div>
-        </div>
+          </div>
+          )
+        }
         <CommentSection showComments={showComments} feedId={post._id} totalCommentsFunc={(commentLen)=>setTotalcomments(commentLen)} showCommentsFunc={()=>setShowComments(!showComments)}/>
     </div>
   )
